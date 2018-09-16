5d01673a767435f45e77139eeeaa11e9a87a2b9c
NULL-LI <lixiang3.1416@qq.com>
2018-09-16 20:39:44 +0800

    edit mesh.cpp

--- Mesh.h
+++ Mesh.h
@@ -1,6 +1,6 @@
-#include <iostream>
 #include <cstdio>
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -8,41 +8,32 @@
 using namespace std;
 class Flight;
 class Mesh;
-string wide_size_names[6] = { "332","333","33E","33H","33L","773" };
-typedef std::vector<Flight*> FlightList;
-enum Size {
-    WIDE,
-    SLIM
-};
-enum FlyType {
-    D,
-    I
-};
-class Flight
-{
+string wide_size_names[6] = {"332", "333", "33E", "33H", "33L", "773"};
+typedef std::vector<Flight *> FlightList;
+enum Size { WIDE, SLIM };
+enum FlyType { D, I };
+class Flight {
 private:
-
 public:
+  int id;
+  int time_arrive;
+  int time_go;
+  Size type_size;      //
+  FlyType type_arrive; //
+  FlyType type_go;
+  string flight_arrive;
+  string flight_go;
 
-    int id;
-    int time_arrive;
-    int time_go;
-    Size type_size;//��խ��
-    FlyType type_arrive;//��������/��������
-    FlyType type_go;
-    string flight_arrive;
-    string flight_go;
-
-    Flight(){}
-    int timeDiff(int time1, int time2) ;
-    Size getTypeSize(string type) ;
+  Flight() {}
+  int timeDiff(int time1, int time2);
+  Size getTypeSize(string type);
 };
 class Mesh {
 public:
-    FlightList flightList;
-    Mesh(){}
-    ~Mesh() { clear(); }
-    void addFlights(string flights_name) ;
-    void clear() ;
-    string Trim(string& str);
+  FlightList flightList;
+  Mesh() {}
+  ~Mesh() { clear(); }
+  void addFlights(string flights_name);
+  void clear();
+  string Trim(string &str);
 };
