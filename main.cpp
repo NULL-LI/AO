8ecf153f25edca761b789ef6b23ea11c3c6a9c2d
yeekal <gangy521@gmail.com>
2018-09-17 13:52:32 +0800

    Auto stash before merge of "master" and "origin/master"

--- main.cpp
+++ main.cpp
@@ -8,23 +8,70 @@ int main()
 	printf("Start main\n");
 	mesh->addFlights("../data/pucks.csv");
     mesh->addGates("../data/gates.csv");
-	FlightList flight_nii;
+
+	FlightList flight_nii,flight_ndd,flight_nid,flight_ndi;
+	FlightList flight_wii,flight_wdd,flight_wid,flight_wdi;
 	for(int i=0;i<mesh->flightListAll.size();i++){
-		if(mesh->flightListAll[i]->plane_size==N  &&
-		mesh->flightListAll[i]->type_arrive==I  &&
-		mesh->flightListAll[i]->type_go==I){
-			flight_nii.push_back(mesh->flightListAll[i]);
-		}//end if
+		if(mesh->flightListAll[i]->plane_size==N){
+			if(mesh->flightListAll[i]->type_arrive==I){
+				if(mesh->flightListAll[i]->type_go==I){
+					flight_nii.push_back(mesh->flightListAll[i]);
+				}
+				else{
+					flight_nid.push_back(mesh->flightListAll[i]);
+				}
+			}else{
+				if(mesh->flightListAll[i]->type_go==I){
+					flight_ndi.push_back(mesh->flightListAll[i]);
+				}
+				else{
+					flight_ndd.push_back(mesh->flightListAll[i]);
+				}
+
+			}
+		}//end if n
+		else{
+			if(mesh->flightListAll[i]->type_arrive==I){
+				if(mesh->flightListAll[i]->type_go==I){
+					flight_wii.push_back(mesh->flightListAll[i]);
+				}
+				else{
+					flight_wid.push_back(mesh->flightListAll[i]);
+				}
+			}else{
+				if(mesh->flightListAll[i]->type_go==I){
+					flight_wdi.push_back(mesh->flightListAll[i]);
+				}
+				else{
+					flight_wdd.push_back(mesh->flightListAll[i]);
+				}
+
+			}
+		}//end if w
 	}//end for
-	cout<<"n_i_i:"<<flight_nii.size()<<endl;
-	shared_ptr<Graph> graph(new Graph(flight_nii));
-	graph->solve();
-	for(int i=0;i<graph->path_list.size();i++){
-		for(int j=0;j<graph->path_list[i].size();j++){
-			cout<<graph->path_list[i][j]->time_go<<" ";
-		}
-		cout<<endl;
+
+	vector<FlightList> flght_groups;
+	flight_groups.push_back(flight_nii);
+	flight_groups.push_back(flight_ndd);
+	flight_groups.push_back(flight_nid);
+	flight_groups.push_back(flight_ndi);
+	flight_groups.push_back(flight_wii);
+	flight_groups.push_back(flight_wdd);
+	flight_groups.push_back(flight_wid);
+	flight_groups.push_back(flight_wdi);
+
+
+	shared_ptr<Graph> graph;
+	printf("%4s%4s%4s%4s%4s%4s%4s%4s\n","wii","wdd","wid","wdi","wii","wdd","wid","wdi");
+	for(int i=0;i<flight_groups.size();i++){
+		graph.reset(new Graph(flight_groups[i]));
+		graph->solve();
+		int path_num=graph->path_list.size();
+		printf("%4d",&path_num);
 	}
+	cout<<endl;
+	mesh->getGateInfo();
+	//cout<<"n i i:"<<mesh->gateInfoAll.gateNum_Narrow_I_I<<endl;
 
 
 
