c30a78122e4032921d629bf60d87617ee9a4c329
NULL-LI <lixiang3.1416@qq.com>
2018-09-17 13:57:46 +0800

    add flight.cpp

--- Mesh.h
+++ Mesh.h
@@ -1,32 +1,19 @@
 
 #include "basiclibs.h"
 #include "gate.h"
+#include "flight.h"
 using namespace std;
 
 #ifndef mesh_h
 #define mesh_h
 
-static string wide_size_names[6] = {"332", "333", "33E", "33H", "33L", "773"};
 
 
-class Flight {
-private:
-public:
-  int id;
-  int time_arrive;
-  int time_go;
-  Size plane_size;      //
-  FlyType type_arrive; //
-  FlyType type_go;
-  string flight_arrive_number;
-  string flight_leave_number;
+//class Flight {
+//
+//};
 
-  Flight() {}
-  int timeDiff(int time1, int time2);
-  Size getTypeSize(string type);
-};
-
-typedef std::vector<shared_ptr<Flight>> FlightList;
+typedef std::vector<shared_ptr<FLIGHT>> FlightList;
 typedef std::vector<shared_ptr<GATE >> GateList;
 
 class Mesh {
